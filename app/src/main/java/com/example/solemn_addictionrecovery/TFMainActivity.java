f307a97b654328b9402bddbde41c38b9859727cf
6479AMS <49047675+6479AMS@users.noreply.github.com>
2021-05-02 22:33:14 +1000

    added labelmap

--- app/src/main/java/com/example/solemn_addictionrecovery/TFMainActivity.java
+++ app/src/main/java/com/example/solemn_addictionrecovery/TFMainActivity.java
@@ -26,7 +26,7 @@ public class TFMainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tfmain);
 
         mainActivity = this;
         try {
